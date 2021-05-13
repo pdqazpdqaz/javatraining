1127bb898e27c3f9d2cb454c452af6b111b7cbf9
pdqazpdqaz <84111446+pdqazpdqaz@users.noreply.github.com>
2021-05-13 11:53:17 +0900

    커밋체인지2
    HelloHello 추가

--- Hellojava.java
+++ Hellojava.java
@@ -3,5 +3,6 @@ public class Hellojava
         public static void main(String[] args)
         {
                  System.out.println("Hello");
+                 System.out.println("HelloHello");
         }
 }
