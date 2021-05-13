d1c4a62b9c4c0dacd7bfbf5bbf66e5c84adbe9db
pdqazpdqaz <84111446+pdqazpdqaz@users.noreply.github.com>
2021-05-13 12:00:05 +0900

    커밋체인지2
    삭제

--- Hellojava.java
+++ Hellojava.java
@@ -2,7 +2,6 @@ public class Hellojava
 {
         public static void main(String[] args)
         {
-                 System.out.println("Hello");
-                 System.out.println("HelloHello");
+                
         }
 }
